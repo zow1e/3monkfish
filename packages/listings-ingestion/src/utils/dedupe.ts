c126f0dbd9af7dfe60fc05404d4d2235ef822aeb
zow1e <zowieong.zo@gmail.com>
2026-03-28 13:15:19 +0800

    remove files

--- packages/listings-ingestion/src/utils/dedupe.ts
+++
@@ -1,8 +0,0 @@
-export const dedupeByKey = <T>(items: readonly T[], getKey: (item: T) => string): T[] => {
-  const seen = new Map<string, T>();
-  for (const item of items) {
-    seen.set(getKey(item), item);
-  }
-
-  return [...seen.values()];
-};
